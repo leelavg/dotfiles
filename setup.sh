c05f07c5668c2be8475a25c7a075188ad9001e9f
glv9_cs50 <glv1092@gmail.com>
2019-11-03 15:26:22 +0000

    Tested installation script on CentOS 7.2

--- setup.sh
+++ setup.sh
@@ -4,21 +4,22 @@
 set -e
 set -o pipefail
 
-ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
-SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
-echo "BEGIN ${SCRIPT_NAME%.sh}"
 
 # Logging to file and displaying on terminal
 # https://unix.stackexchange.com/questions/323142/send-to-log-and-display-on-console
-TIME=`date +%b-%d-%y`
+TIME=`date +%b_%d_%y-%H-%M-%S`
 exec 3<&1
 coproc mytee { tee setup_$TIME.log >&3; }
 exec >&${mytee[1]} 2>&1
 
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+echo "BEGIN ${SCRIPT_NAME%.sh}"
+
 # Download required resources if not exists
-if [[ ! -d $HOME/dotfiles/downloads || -z "$(ls -A $HOME/dotfiles/downloads)" ]]; then
-    mkdir -pv $HOME/dotfiles/downloads
-    cd $HOME/dotfiles/downloads
+if [[ ! -d $ABSOLUTE_PATH/downloads || -z "$(ls -A $ABSOLUTE_PATH/downloads)" ]]; then
+    mkdir -pv $ABSOLUTE_PATH/downloads
+    cd $ABSOLUTE_PATH/downloads
     curl -OL https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
     curl -OL https://github.com/tmux/tmux/releases/download/2.7/tmux-2.7.tar.gz
     git clone https://github.com/tpope/vim-surround.git
@@ -41,21 +42,20 @@ if [[ -n $1 && $1 == 1 ]]; then
     sudo yum -y groupinstall "Development Tools"
     sudo yum -y install glibc-static ncurses-devel
 
-    tar -xvzf $HOME/dotfiles/downloads/libevent-2.1.8-stable.tar.gz
+    tar -xvzf $ABSOLUTE_PATH/downloads/libevent-2.1.8-stable.tar.gz
     cd libevent-2.1.8-stable
     ./configure --prefix=/usr/local
     make
     sudo make install
     cd ..
 
-    tar -xvzf $HOME/dotfiles/downloads/tmux-2.7.tar.gz
+    tar -xvzf $ABSOLUTE_PATH/downloads/tmux-2.7.tar.gz
     cd tmux-2.7
     LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
     make
     sudo make install
     cd ..
 
-    pkill tmux
     rm -rfv libevent-2.1.8-stable
     rm -rfv tmux-2.7
 
@@ -65,51 +65,54 @@ if [[ -n $1 && $1 == 1 ]]; then
 fi
 
 # Start afresh with dotfiles
-if [[ -n $2 && $2 == 1]]; then
+if [[ -n $2 && $2 == 1 ]]; then
 
-    mkdir -pv $HOME/dot_bkp
+    mkdir -pv $ABSOLUTE_PATH/dot_bkp
 
     # Shell
-    mv -fv $HOME/{.profile,.cshrc,.bashrc} $HOME/dot_bkp/
-    ln -sfv $HOME/dotfiles/shell/profile $HOME/.profile
-    ln -sfv $HOME/dotfiles/shell/cshrc $HOME/.cshrc
-    ln -sfv $HOME/dotfiles/shell/bashrc $HOME/.bashrc
+    for rc_file in profile cshrc bashrc
+    do
+        [ -f $HOME/.$rc_file ] && mv -fv $HOME/.$rc_file $ABSOLUTE_PATH/dot_bkp/$rc_file
+        ln -sfv $ABSOLUTE_PATH/shell/$rc_file $HOME/.$rc_file
+    done
 
     # Vim
-    mv -rfv $HOME/.vim* $HOME/dot_bkp/
+    [ -d $HOME/.vim ] && mv -fv $HOME/.vim $ABSOLUTE_PATH/dot_bkp/vim
+    [ -f $HOME/.vimrc ] && mv -fv $HOME/.vimrc $ABSOLUTE_PATH/dot_bkp/vimrc
     rm -rfv $HOME/.vim*
     mkdir -pv $HOME/.vim/pack/bundle/start
     mkdir -pv $HOME/.vim/configs
 
-    for dir in $HOME/dotfiles/downloads/*
+    for dir in $ABSOLUTE_PATH/downloads/*
     do
         dir=`basename $dir`
         if [[ $dir =~ vim ]]; then
-            ln -sfv $HOME/dotfiles/downloads/$dir $HOME/.vim/pack/bundle/start/
+            ln -sfv $ABSOLUTE_PATH/downloads/$dir $HOME/.vim/pack/bundle/start/
         fi
     done
 
-    for file in $HOME/dotfiles/vim/*
+    for file in $ABSOLUTE_PATH/vim/*
     do
         file=`basename $file`
         if [[ $file =~ vimrc ]]; then
-            ln -sfv $HOME/dotfiles/vim/$file $HOME/.$file
+            ln -sfv $ABSOLUTE_PATH/vim/$file $HOME/.$file
         elif [[ $file =~ init ]]; then
-            ln -sfv $HOME/dotfiles/vim/$file $HOME/.config/nvim/
+            [ -d $HOME/.config/nvim ] && mv -fv $HOME/.config/nvim $ABSOLUTE_PATH/dot_bkp/nvim
+            [ -d $HOME/.config/nvim ] && ln -sfv $ABSOLUTE_PATH/vim/$file $HOME/.config/nvim/
         elif [[ $file =~ config ]]; then
-            ln -sfv $HOME/dotfiles/vim/$file $HOME/.vim/
+            ln -sfv $ABSOLUTE_PATH/vim/$file $HOME/.vim/
         else
-            ln -sfv $HOME/dotfiles/downloads/$dir $HOME/.vim/configs/
+            ln -sfv $ABSOLUTE_PATH/vim/$file $HOME/.vim/configs/
         fi
     done
 
     # Tmux
-    mv -fv $HOME/.tmux.conf $HOME/dot_bkp/
-    ln -sfv $HOME/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+    [ -f $HOME/.tmux.conf ] && mv -fv $HOME/.tmux.conf $ABSOLUTE_PATH/dot_bkp/
+    ln -sfv $ABSOLUTE_PATH/tmux/tmux.conf $HOME/.tmux.conf
 
-    FILENAME=dotfile_bakup-$TIME.tar.gz        
-    tar -cpzf $HOME/$FILENAME $HOME/dot_bkp   
-    rm -rf dot_bkp                           
+    FILENAME=dotfile_bakup-$TIME.tar.gz
+    tar -cpzf $FILENAME dot_bkp
+    rm -rf dot_bkp
 
 fi
 
