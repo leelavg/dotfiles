a4c37783e171fbc1b9627c224028babda78083ec
lgangava <leelavg@thoughtexpo.com>
2024-05-21 19:52:14 +0530

    cleanup; move existing prompt from bash to starship

--- .starter.sh
+++ .starter.sh
@@ -6,3 +6,10 @@ function cfg {
 cfg checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv -f {} /tmp/
 cfg checkout
 cfg config status.showUntrackedFiles no
+
+# Tools
+# dnf: foot, helix-editor, ripgrep, bat, fzf, zoxide, podman, buildah, skopeo, bat
+# dnf repo: docker ce, incus, version-fox
+# scripted: startship
+# minor: draw.io, kubefwd, asciinema, tlp, mpv, rlwrap
+# gnome-extensions: bing wallpaper, blur my shell, clipboard indicator, coverflow alt-tab, easyscreencast, gsconnect, no overview at start-up, shortcuts, tactile
